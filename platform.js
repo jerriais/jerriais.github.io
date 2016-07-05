36e99320024aad48ab27a7d0e4a1ffa9a726f57e
Matthew Thornington <Matthew.Thornington@qlik.com>
2016-07-04 22:29:12 -0400

    Add game levels

--- platform.js
+++ platform.js
@@ -21,5 +21,22 @@ function Level(simpleLevelPlan) {
   this.grid = [];
   this.actors = [];
 
-  for var y=0; y
+  for (var y=0; y<this.height; y++) {
+    var line = simpleLevelPlan[y], gridline = [];
+    for (var x = 0; x < this.width; x++){
+      var ch = line[x], fieldType = null;
+      var Actor = actorChars[ch];
+      if (Actor)
+        this.actors.push(new Actor(new Vector(x, y), ch));
+      else if (ch == "x")
+        fieldType = "wall";
+      else if (ch == "!")
+        fieldType = "lava;"
+    }
+    this.grid.push(gridline);
+  }
+  this.player = this.actors.filter(function(actor){
+    return actor.type == "player";
+  })[0];
+  this.staus = this.finishDelay = null;
 }
