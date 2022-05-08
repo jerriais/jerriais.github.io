29d99e4fecb5b40cadea378ef1b148966a88805f
Matt Thornington <mattbonn@gmail.com>
2022-05-08 23:09:50 +0200

    canvas

---
+++ Misc/gamedev/chap2canvas/src/main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+console.log(ctx.canvas);
+
+// const canvas = document.querySelector("#board canvas");
+// const ctx = canvas.getContext("2d");
+// console.log(ctx.canvas);
