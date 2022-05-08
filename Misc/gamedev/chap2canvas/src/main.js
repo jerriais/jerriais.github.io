05070d408a6b1235ade6be184f076c5f89112207
Matt Thornington <mattbonn@gmail.com>
2022-05-08 23:21:09 +0200

    more canvas

--- Misc/gamedev/chap2canvas/src/main.js
+++ Misc/gamedev/chap2canvas/src/main.js
@@ -2,6 +2,17 @@ const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 console.log(ctx.canvas);
 
+ctx.strokeStyle = "black";
+ctx.fillStyle = "red";
+
+ctx.fillRect(300,200,50,50);
+ctx.strokeRect(300,200,50,50);
+
+ctx.beginPath();
+ctx.arc(325,170,25,0,Math.PI *2, false);
+ctx.fill();
+ctx.stroke();
+
 // const canvas = document.querySelector("#board canvas");
 // const ctx = canvas.getContext("2d");
 // console.log(ctx.canvas);
